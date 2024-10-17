5e7eec114b5352326982aae7b0cb02d7953f0992
Kim Jae Ki <jack68@naver.com>
2024-10-17 20:14:59 +0900

    first commit

---
+++ app/src/main/java/com/kt/ybox/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.kt.ybox
+
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+        webView.webViewClient = WebViewClient()
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+
+        webView.loadUrl("http://10.0.2.2:8080/image/uploadImg")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
